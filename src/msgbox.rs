8d68241e1793b10a050c456408570d70774b78f0
kate <kate@dariox.club>
2024-05-13 19:49:26 +0800

    Implement logic for displaying message dialog

---
+++ src/msgbox.rs
@@ -0,0 +1,40 @@
+use native_dialog::{MessageDialog, MessageType};
+pub fn error(code: usize) {
+    message_dialog(
+        crate::text::message(code),
+        "Error".to_string(),
+        MessageType::Error);
+}
+pub fn error_msg(code: usize, msg: String) {
+    let content = crate::text::message(code)
+        .replace("%s", msg.as_str());
+    message_dialog(
+        content,
+        "Error".to_string(),
+        MessageType::Error);
+}
+
+pub fn error_custom(text: String, title: String) {
+    message_dialog(text, title, MessageType::Error);
+}
+pub fn message_dialog(text: String, title: String, msg_type: MessageType) {
+    if let Err(e) = MessageDialog::new()
+        .set_type(msg_type)
+        .set_title(title.as_str())
+        .set_text(text.as_str())
+        .show_confirm() {
+        eprintln!("[msgbox.message_dialog] failed to show dialog: {:#?}", e);
+        eprintln!("[msgbox.message_dialog] failed to show dialog\ntext: {}\ntitle: {}\ntype: {}",
+            text,
+            title,
+            messagetype_as_str(msg_type));
+    }
+}
+
+fn messagetype_as_str(v: MessageType) -> String {
+    match v {
+        MessageType::Info => "Info",
+        MessageType::Warning => "Warning",
+        MessageType::Error => "Error"
+    }.to_string()
+}
